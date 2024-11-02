e3e2baa168b63900ab2067be41033fbd0bbb041b
alejandro-411 <castano.alejandro.3774@eam.edu.co>
2024-11-02 11:57:03 -0500

    service for schedule created, fixed docs in workspace service

--- FastAPI/app/services/workspace_service.py
+++ FastAPI/app/services/workspace_service.py
@@ -6,7 +6,9 @@ from FastAPI.app.models.workspace import Workspace
 from fastapi import Body
 
 class WorkspaceService:
-
+    """
+     Service class for managing workspaces and their schedules.
+    """
     def get_workspaces(self):
         """
         Get all workspaces, including their schedules.
@@ -63,29 +65,3 @@ class WorkspaceService:
         except DoesNotExist:
             return "Workspace not found"
 
-    def add_schedule(self, workspace_id: int, schedule_data: dict):
-        """
-        Add a new schedule to a workspace.
-        """
-        try:
-            workspace = WorkspaceModel.get(WorkspaceModel.id == workspace_id)
-            ScheduleModel.create(
-                openingTime=schedule_data['openingTime'],
-                closingTime=schedule_data['closingTime'],
-                status=schedule_data['status'],
-                workspace=workspace
-            )
-            return "Schedule added successfully"
-        except DoesNotExist:
-            return "Workspace not found"
-
-    def delete_schedule(self, schedule_id: int):
-        """
-        Delete a specific schedule by ID.
-        """
-        try:
-            schedule = ScheduleModel.get(ScheduleModel.id == schedule_id)
-            schedule.delete_instance()
-            return "Schedule deleted successfully"
-        except DoesNotExist:
-            return "Schedule not found"
