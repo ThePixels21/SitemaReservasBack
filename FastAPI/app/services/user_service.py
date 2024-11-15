ea7a6778ae4fe983db948b6cc626bed2909fd888
pipe0427 <pipe.04271@gmail.com>
2024-11-15 15:31:44 -0500

    pylint

--- FastAPI/app/services/user_service.py
+++ FastAPI/app/services/user_service.py
@@ -16,10 +16,11 @@ Imports:
 
 from peewee import DoesNotExist, IntegrityError
 
-from models.person import User # pylint: disable=import-error
-from database import PersonModel # pylint: disable=import-error
-from services.auth_service import get_password_hash # pylint: disable=import-error
+from models.person import User,RoleEnum
+from database import PersonModel
 from fastapi import Body, HTTPException
+from services.auth_service import get_password_hash,get_current_user
+
 
 class UserService:
     """
